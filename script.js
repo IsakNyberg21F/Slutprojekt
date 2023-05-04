29d6bb300418bc7c1070e29191e4632bb2a03ab1
IsakNyberg21F <isak.nyberg@elev.ga.ntig.se>
2023-05-04 18:15:19 +0200

    Fixade filer

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-let cart = [];
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+const input = document.getElementById('quantity-input');
+const input1 = document.getElementById('quantity-input1');
+const input2 = document.getElementById('quantity-input2');
+const input3 = document.getElementById('quantity-input3');
 
-function addToCart() {
-  const title = document.querySelector('.product-title').textContent;
-  const price = document.querySelector('.product-price').textContent;
-  const size = document.querySelector('#size').value;
-  const item = { title, price, size };
-  cart.push(item);
-  alert('Item added to cart!');
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
 }
 
-function viewCart() {
-  console.log(cart);
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
 }
+
+if(input) {
+    input.addEventListener('click', () => {
+        if (input.value < 1) {
+            input.value = 0;
+        }
+})}
+if(input1) {
+    input1.addEventListener('click', () => {
+        if (input1.value < 1) {
+            input1.value = 0;
+        }
+})}
+if(input2) {
+    input2.addEventListener('click', () => {
+        if (input2.value < 1) {
+            input2.value = 0;
+        }
+})}
+if(input3) {
+    input3.addEventListener('click', () => {
+        if (input3.value < 1) {
+            input3.value = 0;
+        }
+})}
+
