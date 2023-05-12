9309c6953c7f5f91f835e2b1d1499dca332c4efa
IsakNyberg21F <isak.nyberg@elev.ga.ntig.se>
2023-05-12 21:49:33 +0200

    fixade cart

--- script.js
+++ script.js
@@ -33,134 +33,126 @@ if(input) {
         }
 })}
 
-var trArray = [];
+var subtotalValue = 0;
 
-function addProduct(currentProduct){
-    fetch('data.json')
-      .then(response => response.json())
-      .then(data => {
-    var sproductPrice = document.getElementById("sproduct-price");
-    var quantityInput =  document.getElementById("quantity-input");
-    console.log(currentProduct)
-    // create <tr> element
-    var tr = document.createElement("tr");
-    tr.id = "tr";
-    console.log(tr.id);
+function addProduct(currentProduct) {
+  fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+      var sproductPrice = document.getElementById("sproduct-price");
+      var quantityInput = document.getElementById("quantity-input");
 
-    // create <td> elements
-    var td1 = document.createElement("td");
-    var td2 = document.createElement("td");
-    var td3 = document.createElement("td");
-    var td4 = document.createElement("td");
-    var td5 = document.createElement("td");
-    var td6 = document.createElement("td");
+      // create <tr> element
+      var tr = document.createElement("tr");
+      tr.id = "tr" + currentProduct;
 
-    // create <a> element with <i> child
-    var a = document.createElement("a");
-    var i = document.createElement("i");
-    i.classList.add("far", "fa-times-circle");
-    a.setAttribute("href", "#");
-    a.appendChild(i);
-    a.id = "fa-times-circle";
-    td1.appendChild(a);
+      // create <td> elements
+      var td1 = document.createElement("td");
+      var td2 = document.createElement("td");
+      var td3 = document.createElement("td");
+      var td4 = document.createElement("td");
+      var td5 = document.createElement("td");
+      var td6 = document.createElement("td");
 
-    // create <img> element
-    var img = document.createElement("img");
-    img.setAttribute("src", "img/products/f" + currentProduct + ".jpg");
-    img.setAttribute("alt", "image");
-    td2.appendChild(img);
+      // create <a> element with <i> child
+      var a = document.createElement("a");
+      var i = document.createElement("i");
+      i.classList.add("far", "fa-times-circle");
+      a.setAttribute("href", "#");
+      a.appendChild(i);
+      a.id = "fa-times-circle";
+      td1.appendChild(a);
 
-    // create text nodes for the remaining <td> elements
-    var td3_text = document.createTextNode("Rayban " + currentProduct);
-    var td4_text = document.createTextNode(sproductPrice.innerText);
-    var td5_input = document.createElement("input");
-    td5_input.setAttribute("type", "number");
-    td5_input.setAttribute("value", quantityInput.value);
-    td5_input.setAttribute("id", "quantity-input" + currentProduct);
-    if(currentProduct === 1){
-        var td6_text = document.createTextNode("$" + data[0].product1.price * quantityInput.value);
-    }
-    else if(currentProduct === 2){
-        var td6_text = document.createTextNode("$" + data[0].product2.price * quantityInput.value);
-    }
-    else if(currentProduct === 3){
-        var td6_text = document.createTextNode("$" + data[0].product3.price * quantityInput.value);
-    }
-    else if(currentProduct === 4){
-        var td6_text = document.createTextNode("$" + data[0].product4.price * quantityInput.value);
-    }
-    else if(currentProduct === 5){
-        var td6_text = document.createTextNode("$" + data[0].product5.price * quantityInput.value);
-    }
-    else if(currentProduct === 6){
-        var td6_text = document.createTextNode("$" + data[0].product6.price * quantityInput.value);
-    }
-    else if(currentProduct === 7){
-        var td6_text = document.createTextNode("$" + data[0].product7.price * quantityInput.value);
-    }
-    else if(currentProduct === 8){
-        var td6_text = document.createTextNode("$" + data[0].product8.price * quantityInput.value);
-    }
+      // create <img> element
+      var img = document.createElement("img");
+      img.setAttribute("src", "img/products/f" + currentProduct + ".jpg");
+      img.setAttribute("alt", "image");
+      td2.appendChild(img);
 
-    // append the <td> elements to the <tr> element
-    td3.appendChild(td3_text);
-    td4.appendChild(td4_text);
-    td5.appendChild(td5_input);
-    td6.appendChild(td6_text);
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    tr.appendChild(td4);
-    tr.appendChild(td5);
-    tr.appendChild(td6);
+      // create text nodes for the remaining <td> elements
+      var td3_text = document.createTextNode("Rayban " + currentProduct);
+      var td4_text = document.createTextNode(sproductPrice.innerText);
+      var td5_input = document.createElement("input");
+      td5_input.setAttribute("type", "number");
+      td5_input.setAttribute("value", quantityInput.value);
+      td5_input.setAttribute("id", "quantity-input" + currentProduct);
+      
+      var price = data[0]["product" + currentProduct].price;
+      var quantity = parseInt(quantityInput.value);
+      var totalPrice = price * quantity;
+      subtotalValue += totalPrice;
+      
+      var td6_text = document.createTextNode("$" + totalPrice);
 
-    // add event listener to remove the row
-    a.addEventListener("click", function() {
-        tr.remove();
-        // remove from localStorage
-        localStorage.removeItem(tr.id);
-    });
+      // append the <td> elements to the <tr> element
+      td3.appendChild(td3_text);
+      td4.appendChild(td4_text);
+      td5.appendChild(td5_input);
+      td6.appendChild(td6_text);
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      tr.appendChild(td3);
+      tr.appendChild(td4);
+      tr.appendChild(td5);
+      tr.appendChild(td6);
 
-    // Store the tr element in localStorage
-    localStorage.setItem(tr.id, tr.outerHTML);
-})
-.catch(error => console.error(error));
+      // Store the tr element in localStorage
+      var cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+      var productExists = cart.some(item => item.includes("tr" + currentProduct));
+
+      if (productExists) {
+        // Product already exists, show a message or handle it accordingly
+        console.log("Product already added to the cart.");
+        return;
+      }
+      cart.push(tr.outerHTML);
+      localStorage.setItem("cart", JSON.stringify(cart));
+      localStorage.setItem("subtotalValue", subtotalValue.toString());
+    })
+    .catch(error => console.error(error));
 }
 
-/*removeButton1 = document.getElementById("fa-times-circle1");
-removeButton2 = document.getElementById("fa-times-circle2");
-removeButton3 = document.getElementById("fa-times-circle3");
-removeButton4 = document.getElementById("fa-times-circle4");
-removeButton5 = document.getElementById("fa-times-circle5");
-removeButton6 = document.getElementById("fa-times-circle6");
-removeButton7 = document.getElementById("fa-times-circle7");
-removeButton8 = document.getElementById("fa-times-circle8");
+function update_subtotal() {
+    var subtotalValue = localStorage.getItem("subtotalValue");
+    console.log(subtotalValue);
+    var subtotalInput = document.getElementById("cart-subtotal");
+    var subtotalInput2 = document.getElementById("cart-subtotal-2");
+    subtotalInput.innerHTML = "$ " + subtotalValue;
+    subtotalInput2.innerHTML = "$ " + subtotalValue;
+}
 
-removeButton1.addEventListener('click', () => {
-    document.getElementById("tr1").remove();
-});
-removeButton2.addEventListener('click', () => {
-    document.getElementById("tr2").remove();
-});
-removeButton3.addEventListener('click', () => {
-    document.getElementById("tr3").remove();
-});
-removeButton4.addEventListener('click', () => {
-    document.getElementById("tr4").remove();
-});
-removeButton5.addEventListener('click', () => {
-    document.getElementById("tr5").remove();
-});
-removeButton6.addEventListener('click', () => {
-    document.getElementById("tr6").remove();
-});
-removeButton7.addEventListener('click', () => {
-    document.getElementById("tr7").remove();
-});
-removeButton8.addEventListener('click', () => {
-    document.getElementById("tr8").remove();
-});*/
+function load_cart() {
+    update_subtotal();
 
+    var tbody = document.getElementById("tbody");
+    var cart = JSON.parse(localStorage.getItem("cart")) || [];
+    
+    cart.forEach(item => {
+    var newTr = document.createElement("tr");
+    newTr.innerHTML = item;
+    tbody.appendChild(newTr);
+
+    // add event listener to remove the row
+    var faTimesCircle = newTr.querySelector("#fa-times-circle");
+    faTimesCircle.addEventListener("click", function() {
+      removeRow(newTr);
+    });
+  });
+}
+
+function removeRow(newTr) {
+    var subtotalValue = localStorage.getItem("subtotalValue");
+    var productPrice = parseFloat(newTr.querySelector("td:nth-child(6)").textContent.slice(1));
+    subtotalValue -= productPrice;
+  
+    newTr.remove();
+    var cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart = cart.filter(item => item !== newTr.outerHTML);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    localStorage.setItem("subtotalValue", subtotalValue.toString());
+    update_subtotal();
+}
 /*
 
  // Hämtar JSON-data från ammodata.json
@@ -202,22 +194,3 @@ removeButton8.addEventListener('click', () => {
      }
  })
  .catch(error => console.error(error)); */
-
-function load_cart(){
-    for (var i = 0; i < localStorage.length; i++) {
-        var tbody = document.getElementById("tbody");
-        var key = localStorage.key(i);
-        var value = localStorage.getItem(key);
-        var newTr = document.createElement('tr');
-        newTr.innerHTML += value;
-        tbody.appendChild(newTr);
-
-        // add event listener to remove the row
-        var faTimesCircle = newTr.querySelector("#fa-times-circle");
-        faTimesCircle.addEventListener("click", function() {
-            newTr.remove();
-            // remove from localStorage
-            localStorage.removeItem(key);
-        });
-    }
-}
