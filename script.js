42c8ce1e5f057f34ce3382dbb418165366417e8b
IsakNyberg21F <isak.nyberg@elev.ga.ntig.se>
2023-05-30 00:17:22 +0200

    fixade responsivitet

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+const nav = document.getElementById('navigation');
 const input = document.getElementById('quantity-input');
 
 if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active');
+        console.log("dsADasdasdadsa");
     })
 }
 
 if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
+        console.log("dsADasdasdadsa");
     })
 }
 
